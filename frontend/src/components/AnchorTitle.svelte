ad74c3d475f38f4a399cc971840fd9f7bc695773
Wout De Puysseleir <woutdp@gmail.com>
2021-03-18 15:07:06 -0700

    Add navigation menu homepage

---
+++ frontend/src/components/AnchorTitle.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let anchor
+</script>
+
+<a href="#{anchor}" id="{anchor}"><slot/></a>
+
+<style lang="stylus">
+    @import '../styles/variables'
+
+    a
+        margin: 0 0 0.5em 0
+        font-weight: 400
+        line-height: 1.2
+        text-transform: capitalize
+        font-size: 2em
+        color: yellow
+        text-decoration none
+
+        &:hover
+            text-decoration underline
+</style>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,4 +1,31 @@
+<script>
+    import AnchorTitle from '../components/AnchorTitle.svelte'
+</script>
+
+<svelte:head>
+    <title>Wout De Puysseleir</title>
+    <meta name="Description" content="Website of Wout De Puysseleir">
+</svelte:head>
+
+<section class="landing center full-page">
+    <h1>Wout De&nbsp;Puysseleir</h1>
+    <h2>Full-Stack Web&nbsp;Developer</h2>
+    <div class="navigation">
+        <a href="#about" class="border">About</a>
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+        <AnchorTitle anchor="about">About</AnchorTitle>
+        <hr>
+        <p>I'm a freelance web developer working fully remote. The sharpest tools in my toolbox are <code>Python (2, 3)</code>, <code>Django</code>, <code>Svelte</code>, <code>Vue</code> & <code>Docker</code>.</p>
+        <p>Born and raised in Belgium, currently living in Vancouver, Canada.</p>
+    </div>
+</section>
+
 <style lang="stylus">
+    @import '../styles/variables'
+
     .landing
         h1
             font-size: 2.8em
@@ -18,27 +45,28 @@
             +above(4)
                 margin-bottom: 20px
                 font-size: 4em
+
+        .navigation
+            display flex
+            margin-top 30px
+            font-weight: 600;
+
+            a
+                text-decoration none
+                font-size 1.1em
+                padding 10px 18px
+                border 2px dashed @color
+                border-radius 5px
+                background-color darken(background, 20%)
+                box-shadow 8px 8px 0 darken(@background-color, 20%)
+                line-height 1em
+                text-transform uppercase
+                transition-duration 1000ms
+
+                &:hover
+                    border-style solid
+                    color lighten(cyan-bright, 20%)
+
+                &:not(:last-child)
+                    margin-right 20px
 </style>
-
-<svelte:head>
-    <title>Wout De Puysseleir</title>
-    <meta name="Description" content="Website of Wout De Puysseleir">
-</svelte:head>
-
-<section class="landing center full-page">
-    <h1>Wout De&nbsp;Puysseleir</h1>
-    <h2>Full-Stack Web&nbsp;Developer</h2>
-</section>
-<section class="section">
-    <div class="container">
-        <h1>About</h1>
-        <hr>
-        <p>I'm a freelance web developer working fully remote. The sharpest tools in my toolbox are <code>Python (2, 3)</code>, <code>Django</code>, <code>Vue</code> & <code>Docker</code>.</p>
-        <p>
-            When I'm not programming you can see me hiking up a mountain,
-            adapting more of my tools to the <a rel="noopener" target="_blank" href="https://github.com/morhetz/gruvbox-contrib">Gruvbox</a> colorscheme,
-            drinking a cocktail or watching a movie.
-        </p>
-        <p>Born and raised in Belgium, living in Vancouver, Canada.</p>
-    </div>
-</section>
